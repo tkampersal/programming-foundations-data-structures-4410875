e3fad5aa9475512fbb512a206ed0e22a81ccecfc
tkampersal <37404668+tkampersal@users.noreply.github.com>
2024-03-07 16:29:21 +0000

    Challenge

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,5 +1,13 @@
 def has_unique_characters(data):
-    return False
+    letter_set = set([ltr for ltr in data]) 
+    letter_set2 = set(data)
+    print(letter_set)
+    print(letter_set2)
+    if len(data) == len(letter_set):
+        print(f"'{data}' has unique characters!!!")
+    else:
+        print("'" + data + "'" + "DOES NOT have unique characters!!!")
+
 
 print(has_unique_characters('sample'))
 print(has_unique_characters('hello world'))
